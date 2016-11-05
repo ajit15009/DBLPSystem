794d4701beee0705f31ff8ee4965424d8d0b9968
hp <hp@LAPTOP-6QIC50TJ>
2016-11-05 21:20:25 +0530

    added some functions

--- APProject-DBLP/src/Author.java
+++ APProject-DBLP/src/Author.java
@@ -1,11 +1,33 @@
 import java.util.*;
 
-public class Author {
+public class Author implements Comparable<Author> {
 	String name;
 	List<Book>Publications;
 	public Author(String n)
 	{
 		name=n;
 	}
-	
+	public String get_name()
+	{
+		return name;
+	}
+	public void add_publication(Book B)
+	{
+		Publications.add(B);
+	}
+	public int compareTo(Author a)
+	{
+		if(this.name.equals(a.get_name()))
+			return 1;
+		else if(this.name.toLowerCase().contains(a.get_name().toLowerCase()))
+			return 1;
+		else if(a.get_name().toLowerCase().contains(this.name.toLowerCase()))
+			return 1;
+		else return 0;
+	}
+	public static void main(String[] args)
+	{
+		System.out.println("Hello");
+	}
+
 }
