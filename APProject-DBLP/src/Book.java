be94b230534302de96ee9e27de8c505abaa47e79
hp <hp@LAPTOP-6QIC50TJ>
2016-11-05 18:42:19 +0530

    added book class

---
+++ APProject-DBLP/src/Book.java
@@ -0,0 +1,19 @@
+
+public class Book {
+	String title,url;
+	int year,volume,pages;
+	public Book(String t,int p,int y,int v,String u)
+	{
+		year=y;
+		url=u;
+		volume=v;
+		pages=p;
+		title=t;
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
