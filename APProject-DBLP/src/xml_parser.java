9961bf02d1f82cf94d4536c3858498a56665e9c8
sarthak garg <sarthakgarg6@gmail.com>
2016-11-07 00:27:54 +0530

    check it out why it is displaying less results

--- APProject-DBLP/src/xml_parser.java
+++ APProject-DBLP/src/xml_parser.java
@@ -5,12 +5,13 @@ import javax.xml.stream.*;
 import javax.xml.stream.events.XMLEvent;
 import javax.xml.stream.events.*;
 public class xml_parser {
+	int i=0;
 
 	public static void main(String[] args) {
 		XMLInputFactory factory = XMLInputFactory.newInstance();
 		try {
 			XMLEventReader eventReader = factory.createXMLEventReader(new FileReader("dblp.xml"));
-		 while(eventReader.hasNext())
+		 while(i<100)
 		 {
 			 XMLEvent event = eventReader.nextEvent();
 			 if(event.getEventType() == XMLEvent.START_ELEMENT){
