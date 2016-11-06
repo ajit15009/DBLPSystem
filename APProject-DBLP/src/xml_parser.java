a82b3a3e38eba09e3cd42bdd197b5f93e0192833
sarthak garg <sarthakgarg6@gmail.com>
2016-11-07 00:14:07 +0530

    just testing it
    reading the xml file...

--- APProject-DBLP/src/xml_parser.java
+++ APProject-DBLP/src/xml_parser.java
@@ -3,15 +3,20 @@ import java.util.*;
 import java.io.*;
 import javax.xml.stream.*;
 import javax.xml.stream.events.XMLEvent;
+import javax.xml.stream.events.*;
 public class xml_parser {
 
 	public static void main(String[] args) {
 		XMLInputFactory factory = XMLInputFactory.newInstance();
 		try {
-			XMLEventReader eventReader = factory.createXMLEventReader(new FileReader("input.txt"));
+			XMLEventReader eventReader = factory.createXMLEventReader(new FileReader("dblp.xml"));
 		 while(eventReader.hasNext())
 		 {
 			 XMLEvent event = eventReader.nextEvent();
+			 if(event.getEventType() == XMLEvent.START_ELEMENT){
+			        StartElement startElement = event.asStartElement();
+			        System.out.println(startElement.getName());
+			    }
 		 }
 		}
 		catch(FileNotFoundException | XMLStreamException e) {
